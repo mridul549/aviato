07c5beedc23fd5bc9a34596385a78ebac252998f
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2024-04-24 01:43:22 +0530

    Update: Add sort by functionality for videos

--- routes/video.py
+++ routes/video.py
@@ -8,18 +8,27 @@ video = APIRouter()
 @video.get('/')
 async def getVideos(
     page: int = Query(default=1, ge=1),
-    page_size: int = Query(default=10, ge=1)
+    page_size: int = Query(default=10, ge=1),
+    sort_by: int = Query(default=0, ge=0, le=1)
 ):
     # Calculate offset
     offset = (page - 1) * page_size
 
     # Prepare the query for videos with ordering and pagination
-    videos_query = (
-        select(videos)
-        .order_by(videos.c.publishedTime.desc()) 
-        .limit(page_size)
-        .offset(offset)
-    )
+    if sort_by == 0:
+        videos_query = (
+            select(videos)
+            .order_by(videos.c.publishedTime.desc()) 
+            .limit(page_size)
+            .offset(offset)
+        )
+    else: 
+        videos_query = (
+            select(videos)
+            .order_by(videos.c.publishedTime.asc()) 
+            .limit(page_size)
+            .offset(offset)
+        )
 
     # Prepare the query for total count
     count_query = select(func.count()).select_from(videos)
